0fe87df1a8a6337f6d0d07c7407d95d44dfd0230
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-09-01 16:15:48 -0300

    feat: like and dislike

--- tindevmobile/src/routes.js
+++ tindevmobile/src/routes.js
@@ -1,9 +1,9 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Login from './pages/Login';
 import Main from './pages/Main';
 
 export default createAppContainer(
-    createStackNavigator({
+    createSwitchNavigator({
         Login,
         Main
     })
